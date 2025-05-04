da91f1b50474440310aa1bbe84a2ee0f6352c32f
PyMarcus <marcus-v@outlook.com>
2025-05-04 19:48:16 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NasaAstronomyApp());
+}
+
+class NasaAstronomyApp extends StatelessWidget {
+  const NasaAstronomyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: "Nasa Astronomy", home: null);
+  }
+}
